3a9e0abef3dc201af481c095f30f109669ae0101
Tom Ashworth <tom@phuu.net>
2012-08-06 19:55:50 +0100

    Use process.env.PORT for Heroku

--- app.js
+++ app.js
@@ -88,5 +88,5 @@ app.get('/auth/twitter/callback', function(req, res, next) {
 
 
 
-app.listen(3000);
+app.listen(process.env.PORT);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
